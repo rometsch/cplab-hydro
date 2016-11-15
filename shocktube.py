resolved error in advection step. it was due to wrong calculation of enerfy flux. used momentum flux instead of mass flux.

shocktube.py
@@ -32,7 +32,8 @@ class Box():
         self.dx = (self.I[1] - self.I[0])/self.N;
         # Variables:
         self.t = 0; # integration time
-        self.X = np.zeros(self.N+4);
+        self.Xa = np.zeros(self.N+4);
+        self.Xb = np.zeros(self.N+4);
         # Use two ghost cells on each side.
         # Allocate array for flux to be calculated by chosen method.
         self.rho = np.zeros(self.N+4);
@@ -41,14 +42,15 @@ class Box():
         self.delta = np.zeros(self.N+4);
         self.flux = np.zeros(self.N+4);
         # Initialize grid and function:
-        self.X[2:-2] = self.I[0] + self.dx*np.arange(self.N);
-        self.update_boundary(self.X);
-#        self.rho[2:-2] = self.rho0(self.X[2:-2]);
-#        self.u[2:-2] = self.u0(self.X[2:-2]);
-#        self.eps[2:-2] = self.eps0(self.X[2:-2]);
-        for f,f0 in ((self.rho,self.rho0),(self.u,self.u0),(self.eps,self.eps0)):
-            f[2:-2] = f0(self.X[2:-2]);
-        self.update_boundary_all();   
+        self.Xa = self.I[0] + self.dx*(-2+np.arange(self.N+4));
+        self.Xb = self.Xa +0.5*self.dx;
+        self.rho[2:-2] = self.rho0(self.Xb[2:-2]);
+        self.update_boundary_rho(self.rho);
+        self.u[2:-2] = self.u0(self.Xa[2:-2]);
+        self.update_boundary_velocity(self.u);
+        self.eps[2:-2] = self.eps0(self.Xb[2:-2]);
+        self.update_boundary_eps(self.eps);
+
         
   
   
@@ -65,61 +67,80 @@ class Box():
         
     def integration_step(self):
         '''Manage advection of density, velocity and energy.'''
-        
+        N = self.N;
         # Advection with 2nd order Upwind scheme
-        rho_new = np.zeros(self.N+4);
-        flux_mass = np.zeros(self.N+4);
-        flux_mass[2:-1] = self.advection_scalar(self.rho)[2:-1]*self.u[2:-1];  
-        rho_new[2:-2] = self.rho[2:-2]-self.dt/self.dx*(flux_mass[3:-1]-flux_mass[2:-2]);
-        self.update_boundary_rho(rho_new);
+        rho_new = np.zeros(N+4);
+        flux_mass = np.zeros(N+4);
+        flux_mass[2:N+3] = self.advection_scalar(self.rho)[2:N+3]*self.u[2:N+3];  
+        rho_new[2:N+2] = self.rho[2:N+2]-self.dt/self.dx*(flux_mass[3:N+3]-flux_mass[2:N+2]);
+#        self.update_boundary_rho(rho_new);
 
-        u_new = np.zeros(self.N+4);
-        flux_momentum = np.zeros(self.N+4);
-        flux_momentum[2:-2] = 0.5*self.advection_velocity(self.u)[2:-2]*(flux_mass[2:-2]+flux_mass[3:-1]);
-        rho_avg = np.zeros(self.N+4);
-        rho_avg_new = np.zeros(self.N+4);
-        rho_avg[3:-2] = 0.5*(self.rho[2:-3]+self.rho[3:-2]);
-        rho_avg_new[3:-2] = 0.5*(rho_new[2:-3]+rho_new[3:-2]);
-        if (rho_avg_new[3:-2]==0).any():
+        u_new = np.zeros(N+4);
+        flux_momentum = np.zeros(N+4);
+        flux_momentum[2:N+2] = 0.5*self.advection_velocity(self.u)[2:N+2]*(flux_mass[2:N+2]+flux_mass[3:N+3]);
+        rho_avg_new = self.average_density(rho_new);
+        rho_avg_old = self.average_density(self.rho);        
+        if (rho_avg_new[3:N+2]==0).any():
             print("t = {:.3e} : Division by zero encountered in advection of velcoity".format(self.t));
-        u_new[3:-2] = (self.u[3:-2]*rho_avg[3:-2]-self.dt/self.dx*(flux_momentum[3:-2]-flux_momentum[2:-3]))/rho_avg_new[3:-2];      
-        self.update_boundary_velocity(u_new);        
+        u_new[3:N+2] = (self.u[3:N+2]*rho_avg_old[3:N+2]-self.dt/self.dx*(flux_momentum[3:N+2]-flux_momentum[2:N+1]))/rho_avg_new[3:N+2];      
+#        u_new[3:-2] = (self.u[3:-2]*0.5*(self.rho[2:-3]+self.rho[3:-2])-self.dt/self.dx*(flux_momentum[3:-2]-flux_momentum[2:-3]))/0.5/(rho_new[2:-3]+rho_new[3:-2]);      
+#        self.update_boundary_velocity(u_new);        
         
         eps_new = np.zeros(self.N+4);
         flux_eps = np.zeros(self.N+4);
-        flux_eps[2:-1] = flux_momentum[2:-1]*self.advection_scalar(self.eps)[2:-1];
+        flux_eps[2:-1] = flux_mass[2:-1]*self.advection_scalar(self.eps)[2:-1];
         if (rho_new[2:-2]==0).any():
             print("t = {:.3e} : Division by zero encountered in advection of energy".format(self.t));
         eps_new[2:-2] = (self.eps[2:-2]*self.rho[2:-2]-self.dt/self.dx*(flux_eps[3:-1]-flux_eps[2:-2]))/rho_new[2:-2];
-        self.update_boundary_eps(eps_new);
+#        self.update_boundary_eps(eps_new);
         
         # force and pressure terms
-        p = np.zeros(self.N+4);
-        p[2:-2] = (self.gamma-1)*rho_new[2:-2]*eps_new[2:-2];
+        p = self.calc_pressure(rho_new,eps_new);
         if (rho_avg_new[3:-2]==0).any():
             print("t = {:.3e} : Division by zero encountered force and pressure calculation for velocity".format(self.t));
         self.u[3:-2] = u_new[3:-2] - self.dt/self.dx*(p[3:-2]-p[2:-3])/rho_avg_new[3:-2];
-        self.update_boundary_velocity(self.u);
+#        self.update_boundary_velocity(self.u);
 
         self.eps[2:-2] = eps_new[2:-2]*(1 - self.dt/self.dx*(self.gamma-1)*(u_new[3:-1]-u_new[2:-2]));        
-        self.update_boundary_eps(self.eps);
+#        self.eps[2:-2] = eps_new[2:-2] - self.dt/self.dx*p[2:-2]/rho_new[2:-2]*(u_new[3:-1]-u_new[2:-2]);        
+#        self.update_boundary_eps(self.eps);
 
-        self.rho = rho_new;        
+        self.rho = rho_new;
+        self.update_boundary_rho(self.rho);
+        self.update_boundary_velocity(self.u);
+        self.update_boundary_eps(self.eps);
+        
+    def calc_pressure(self,rho,eps):
+        '''Calculate the pressure at the cell center'''
+        p = np.zeros(self.N+4);
+        p[:] = (self.gamma-1)*rho[:]*eps[:];
+        return p;
+        
+    def average_density(self,rho):
+        '''Calculate average density for cell boundaries'''
+        rho_avg = np.zeros(self.N+4);
+        rho_avg[1:] = 0.5*(rho[1:]+rho[:-1]);
+        return rho_avg;
+        
+    def average_velocity(self,u):
+        '''Calcualte average velocity at cell center'''
+        u_avg = np.zeros(self.N+4);
+        u_avg[:-1] = 0.5*(u[:-1]+u[1:]);
+        return u_avg;
 
     def advection_velocity(self,Y):
         '''Calculate the flux of a vector variable using the
         2nd order upwind scheme'''
-        avg = np.zeros(self.N+4);
         adv = np.zeros(self.N+4);
         delta = self.undivided_difference(Y);
         # calculate average at cell boundary:
-        avg[1:-1] = 0.5*(Y[2:]+Y[1:-1]);
+        u_avg = self.average_velocity(self.u);
         # calculate advected quantities:
         for n in range(2,self.N+2):
-            if avg[n] > 0 :
-                adv[n] = Y[n]+0.5*(1-avg[n]*self.dt/self.dx)*delta[n];
+            if u_avg[n] > 0 :
+                adv[n] = Y[n]+0.5*(1-u_avg[n]*self.dt/self.dx)*delta[n];
             else:
-                adv[n] = Y[n+1]-0.5*(1+avg[n]*self.dt/self.dx)*delta[n+1];
+                adv[n] = Y[n+1]-0.5*(1+u_avg[n]*self.dt/self.dx)*delta[n+1];
         # return resulting flux:        
         return adv;
         
@@ -162,28 +183,19 @@ class Box():
     def update_boundary_rho(self,rho):
         '''Update ghost cells of rho.'''
         rho[1] = rho[2];
-        rho[-1] = rho[-2];
+        rho[self.N+2] = rho[self.N+1];
 
     def update_boundary_velocity(self,u):
         '''Update ghost cells of u.'''
         u[1] = -u[3];
         u[2] = 0;
-        u[-1] = -u[-3];
-        u[-2] = 0;
+        u[self.N+3] = -u[self.N+1];
+        u[self.N+2] = 0;
         
     def update_boundary_eps(self,eps):
         '''Update ghost cells of eps'''
         eps[1] = eps[2];
-        eps[-2] = eps[-3];
-
-    def update_boundary(self,arr):
-        ''' Update ghost cells of variable stored in arr. 
-        Use periodic boundary conditions. '''
-        # Update ghost cells:
-        arr[0] = arr[-4];
-        arr[1] = arr[-3];
-        arr[-2] = arr[2];
-        arr[-1] = arr[3];
+        eps[self.N+2] = eps[self.N+1];
             
     def get_rho(self):
         '''Return array with mass density values.'''
@@ -206,5 +218,5 @@ class Box():
         return (self.gamma-1)*self.eps[2:-2];
         
     def get_x(self):
-        return self.X[2:-2];
+        return self.Xb[2:-2];
             